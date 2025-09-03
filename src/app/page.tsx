d65876840929d7ea98ebb0db660da330efcd9128
Syed Ali Quadri <syedaliquadri100@gmail.com>
2025-09-03 21:43:37 +0530

    Update Home component to include PlanByInterest and enhance layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import HereSection from "@/components/main-page/HeroSection";
+import PlanByInterest from "@/components/main-page/PlanByInterest";
 import React from "react";
 
 const Home = () => {
+	const click = () => {
+		console.log("click");
+	};
 	return (
-    <div className="home">
-      <HereSection />
-    </div>
-  );
+		<div className="home w-full h-screen bg-amber-100">
+			<HereSection />
+			<PlanByInterest />
+		</div>
+	);
 };
 
 export default Home;
