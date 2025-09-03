5b04b76ef3c5e72ddfa3fb23a6a997b266639beb
Syed Ali Quadri <syedaliquadri100@gmail.com>
2025-09-03 21:43:30 +0530

    Add PlanByInterest component for displaying destination options based on user preferences

---
+++ src/components/main-page/PlanByInterest.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import React from "react";
+import Circle from "./mini-components/Circle";
+
+const PlanByInterest = () => {
+	const circleData = [
+		{
+			imageSrc:
+				"https://images.pexels.com/photos/13979893/pexels-photo-13979893.jpeg",
+			title: "Circle Title",
+			link: "https://www.example.com"
+		},
+		{
+			imageSrc:
+				"https://images.pexels.com/photos/13979893/pexels-photo-13979893.jpeg",
+			title: "Circle Title",
+			link: "https://www.example.com"
+		},
+		{
+			imageSrc:
+				"https://images.pexels.com/photos/13979893/pexels-photo-13979893.jpeg",
+			title: "Circle Title",
+			link: "https://www.example.com"
+		},
+		{
+			imageSrc:
+				"https://images.pexels.com/photos/13979893/pexels-photo-13979893.jpeg",
+			title: "Circle Title",
+			link: "https://www.example.com"
+		},
+		{
+			imageSrc:
+				"https://images.pexels.com/photos/13979893/pexels-photo-13979893.jpeg",
+			title: "Circle Title",
+			link: "https://www.example.com"
+		},
+		{
+			imageSrc:
+				"https://images.pexels.com/photos/13979893/pexels-photo-13979893.jpeg",
+			title: "Circle Title",
+			link: "https://www.example.com"
+		},
+		{
+			imageSrc:
+				"https://images.pexels.com/photos/13979893/pexels-photo-13979893.jpeg",
+			title: "Circle Title",
+			link: "https://www.example.com"
+		},
+	];
+
+	return (
+		<div className="w-full h-120 bg-[#FFFFFF] flex flex-col items-center justify-center">
+			<div className="elem-01">
+				<h1 className="text-3xl font-semibold text-center tracking-tight">
+					<em>Plan By Interest</em>
+				</h1>
+				<p className="text-lg text-gray-600 mt-4 font-semibold text-center tracking-tight">
+					Explore destinations based on your mood and preferences.
+				</p>
+			</div>
+			<div className="elem-02 mt-10 flex gap-20">
+				{circleData.map((circle, index) => (
+					<Circle
+						key={index}
+						imageSrc={circle.imageSrc}
+						title={circle.title}
+						link={circle.link}
+					/>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default PlanByInterest;
