6de5a92a18387761a04b83652fbe4ba321595ed4
Syed Ali Quadri <syedaliquadri100@gmail.com>
2025-09-03 21:49:14 +0530

    Add JoinTheTravelRevolution component to Home for enhanced user engagement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import HereSection from "@/components/main-page/HeroSection";
+import JoinTheTravelRevolution from "@/components/main-page/JoinTheTravelRevolution";
 import PlanByInterest from "@/components/main-page/PlanByInterest";
 import React from "react";
 
@@ -10,6 +11,7 @@ const Home = () => {
 		<div className="home w-full h-screen bg-amber-100">
 			<HereSection />
 			<PlanByInterest />
+			<JoinTheTravelRevolution />
 		</div>
 	);
 };
