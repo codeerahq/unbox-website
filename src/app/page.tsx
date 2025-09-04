0638d056243f019605981866d50465ada633d79f
Syed Ali Quadri <syedaliquadri100@gmail.com>
2025-09-04 11:18:02 +0530

    Import UnSpokenTruth component in Home for enhanced user experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 import HereSection from "@/components/main-page/HeroSection";
 import JoinTheTravelRevolution from "@/components/main-page/JoinTheTravelRevolution";
 import PlanByInterest from "@/components/main-page/PlanByInterest";
+import UnSpokenTruth from "@/components/main-page/UnSpokenTruth";
 import React from "react";
 
 const Home = () => {
-	const click = () => {
-		console.log("click");
-	};
 	return (
-		<div className="home w-full h-screen bg-amber-100">
+		<div className="home w-full h-screen overflow-x-hidden ">
 			<HereSection />
 			<PlanByInterest />
 			<JoinTheTravelRevolution />
+			<UnSpokenTruth />
 		</div>
 	);
 };
